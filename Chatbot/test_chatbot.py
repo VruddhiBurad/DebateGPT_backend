ff908007a1231c415d03e2843bf4bdbb2b5cbbf0
Siddhi badgujar <siddhibadgujar18@gmail.com>
2026-01-10 11:39:25 +0530

    Delete Chatbot directory

--- Chatbot/test_chatbot.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://localhost:8000/chat"
-payload = {
-    "message": "Artificial Intelligence will replace human jobs."
-}
-
-response = requests.post(url, json=payload)
-print(response.json()["reply"])
